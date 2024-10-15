c202e51662d452271204362253d413005780dd74
restonic4 <marcosruizlerma@gmail.com>
2024-10-15 10:24:08 +0200

    Created

---
+++ src/main/java/com/restonic4/memory_cleaner/MemoryCleaner.java
@@ -0,0 +1,10 @@
+package com.restonic4.memory_cleaner;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MemoryCleaner implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
