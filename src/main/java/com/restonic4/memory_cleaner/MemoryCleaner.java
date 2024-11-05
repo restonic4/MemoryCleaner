2ca62cb9b8336a22f645fe2877051558bd5c6aa9
restonic4 <marcosruizlerma@gmail.com>
2024-11-05 10:59:06 +0100

    idk

--- src/main/java/com/restonic4/memory_cleaner/MemoryCleaner.java
+++ src/main/java/com/restonic4/memory_cleaner/MemoryCleaner.java
@@ -8,6 +8,8 @@ import net.minecraft.server.level.ServerPlayer;
 import static net.minecraft.commands.Commands.literal;
 
 public class MemoryCleaner implements ModInitializer {
+    private static final double MEMORY_THRESHOLD = 0.85;
+
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("cleanup")
@@ -15,11 +17,34 @@ public class MemoryCleaner implements ModInitializer {
                 .executes(context -> {
                     context.getSource().sendSuccess(() -> Component.literal("RAM cleaned"), true);
 
+                    isMemorySafe();
                     cleanup();
 
                     return 1;
                 })
         ));
+    }
+
+    public static boolean isMemorySafe() {
+        Runtime runtime = Runtime.getRuntime();
+
+
+
+        long maxMemory = runtime.maxMemory();
+        long totalMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+        long usedMemory = totalMemory - freeMemory;
+
+        double usage = (double) usedMemory / maxMemory;
+
+        double maxMemoryGB = maxMemory / (1024.0 * 1024.0 * 1024.0);
+        double totalMemoryGB = totalMemory / (1024.0 * 1024.0 * 1024.0);
+        double freeMemoryGB = freeMemory / (1024.0 * 1024.0 * 1024.0);
+        double usedMemoryGB = usedMemory / (1024.0 * 1024.0 * 1024.0);
+
+        System.out.println(maxMemoryGB + ", " + totalMemoryGB + ", " + freeMemoryGB + ", " + usedMemoryGB + ", " + usage);
+
+        return usage >= MEMORY_THRESHOLD;
     }
 
     public static void cleanup() {
